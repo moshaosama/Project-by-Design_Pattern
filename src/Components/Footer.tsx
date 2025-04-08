d11e6f2c28036516e0b854c85b18a36c902731b7
moshaosama <mohamedOSFekry@gmail.com>
2025-04-08 04:13:24 +0200

    Update Footer component

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div className="bg-gray-400 p-3">
+      <div className="bg-gray-400 p-3 fixed bottom-0 w-full">
         <h1 className="text-md font-semibold text-center">
           Copyright © 2012 - 2025 TermsFeed®. All rights reserved.
         </h1>

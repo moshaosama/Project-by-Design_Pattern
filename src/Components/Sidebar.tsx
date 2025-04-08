fbea78006cab7dbeeb06e1f8e863aea5f72ccf48
moshaosama <mohamedOSFekry@gmail.com>
2025-04-08 04:28:27 +0200

    Update Sidebar component

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -2,7 +2,7 @@ const Sidebar = () => {
   return (
     <div className="bg-gray-700 h-[86.8vh] p-10">
       <div className="flex flex-col gap-10">
-        <a href="/home" className="text-xl font-semibold ">
+        <a href="/" className="text-xl font-semibold ">
           Home
         </a>
 

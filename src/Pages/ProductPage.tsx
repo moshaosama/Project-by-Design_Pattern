773efa77975d9cb8cc71760b13c8562943fd9573
moshaosama <mohamedOSFekry@gmail.com>
2025-04-08 05:41:00 +0200

    Modified: src/Pages/ProductPage.tsx

--- src/Pages/ProductPage.tsx
+++ src/Pages/ProductPage.tsx
@@ -1,12 +1,17 @@
 import ProductByIdLoader from "../Components/Shared/ProductByIdLoader";
 import ProductbyidInfo from "../Components/ProductbyidInfo";
 import { useParams } from "react-router";
+import useLoading from "../Hooks/useLoading";
+import WithLoading from "../Utils/WithLoading";
+
+const WrapperComponent = WithLoading(ProductbyidInfo);
 
 const ProductPage = () => {
+  const { loading } = useLoading();
   const { id } = useParams();
   return (
     <ProductByIdLoader resourceName="product" productId={id as any}>
-      <ProductbyidInfo />
+      <WrapperComponent loading={loading} />
     </ProductByIdLoader>
   );
 };

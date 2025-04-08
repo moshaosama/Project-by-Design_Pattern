b218e22451ebc01ef56d11caaac232ee59de0196
moshaosama <mohamedOSFekry@gmail.com>
2025-04-08 03:00:40 +0200

    Update Sidebar.tsx: added new menu items and refined layout styling

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,7 +1,23 @@
 const Sidebar = () => {
   return (
-    <div>
-      <h1>SideBar</h1>
+    <div className="bg-gray-700 h-[92vh] p-10">
+      <div className="flex flex-col gap-10">
+        <a href="/home" className="text-xl font-semibold ">
+          Home
+        </a>
+
+        <a href="/home" className="text-xl font-semibold ">
+          About
+        </a>
+
+        <a href="/home" className="text-xl font-semibold ">
+          Content
+        </a>
+
+        <a href="/home" className="text-xl font-semibold ">
+          LogOut
+        </a>
+      </div>
     </div>
   );
 };

b6450c8fd6d2af848af75c2af8fe125328e77102
moshaosama <mohamedOSFekry@gmail.com>
2025-04-28 13:40:47 +0300

    feat: update CommentInfo component

--- src/Components/DataInfo/CommentInfo.tsx
+++ src/Components/DataInfo/CommentInfo.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import Modal from "../Modal";
+
 interface commentInfoData {
   comment?: {
     name: string;
@@ -7,14 +10,35 @@ interface commentInfoData {
 }
 
 const CommentInfo = ({ comment }: commentInfoData) => {
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
   return (
     <>
       <div className="grid grid-cols-3 mt-2 gap-5 overflow-y-scroll h-[86.8vh]">
         {comment?.map((comment, index) => (
-          <div className="border-[1px] border-gray-300">
-            <div className="flex gap-3">
+          <div className="border-[1px] border-gray-300 p-5">
+            <div className="flex gap-3 justify-between">
               <p className="font-bold">Comment:</p>
               <h1 key={index}>{comment.name}</h1>
+              <button
+                className="border-[1px] border-solid cursor-pointer"
+                onClick={handleOpenModal}
+              >
+                Open Modal
+              </button>
+
+              {openModal && (
+                <Modal>
+                  <Modal.Title>Modal 1</Modal.Title>
+                  <Modal.Paragraph>Modal 1</Modal.Paragraph>
+                  <Modal.Buttons>
+                    <button>Cancel</button>
+                    <button>Edit</button>
+                  </Modal.Buttons>
+                </Modal>
+              )}
             </div>
           </div>
         ))}
